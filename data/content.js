08c7596bd829ef3ae8b5b3d5cfca22317c07617d
Primož Hadalin <hadalin.primoz@gmail.com>
2014-12-24 09:30:55 +0100

    Add report button

--- data/content.js
+++ data/content.js
@@ -1,12 +1,67 @@
 var bannedProfiles = self.options.bannedProfiles;
 
+
+var getParentUrl = function() {
+	var isInIFrame = (parent !== window),
+        parentUrl = null;
+
+    if(isInIFrame) {
+        parentUrl = document.referrer;
+    }
+
+    return parentUrl;
+};
+
+var submitReport = function(profileId, comment) {
+	$.ajax({
+		url: 'https://jhvisser.com/hidefedora/html/submit/submit.php',
+		type: 'POST',
+		data: {
+			submit: 1,
+			profileUrl: profileId,
+			comment: comment,
+			youtubeUrl: getParentUrl()
+		}
+	});
+};
+
+var onReportClick = function() {
+	var profileId = $(this).data("profileId"),
+		comment = $(this).data("comment"),
+		thisEl = $(this);
+
+	submitReport(profileId, comment);
+
+	thisEl.prop('disabled', true).html('Reported').addClass('hide-fedora-reported');
+
+	setTimeout(function() {
+		thisEl.remove();
+	}, 1000);
+};
+
 var removeFedora = function(outerSelector, innerSelector) {
 	$(outerSelector).each(function(index, element) {
 		var el = $(element),
-			profileId = el.find('[oid]').first().attr('oid');
+			profileId = el.find('[oid]').first().attr('oid'),
+			comment = el.find('div.Ct').first().text(),
+			thisEl = $(this);
 
 		if(_.contains(bannedProfiles, profileId)) {
 			$(this).remove();
+		}
+		else {
+			if(!thisEl.hasClass("hide-fedora-tagged")) {
+				thisEl.addClass("hide-fedora-tagged");
+				thisEl
+					.find('.RN.f8b')
+					.first()
+					.after('<button type="button" class="hide-fedora-report-btn">Report</button>');
+
+				thisEl.find('.hide-fedora-report-btn')
+					.data('profileId', profileId)
+					.data('comment', comment)
+					.click(onReportClick);
+			}
 		}
 	});
 };
